c3808ca5f0d7563fdf5ca5d89eebdeeae9a2f142
Zohaib Ashraf <zohaibashraf2559@gmail.com>
2025-08-02 14:42:25 +0500

    Add files via upload

---
+++ Proj8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int length, width, area;
+
+    cout << "Enter length: ";
+    cin >> length;
+
+    cout << "Enter width: ";
+    cin >> width;
+    
+    area = length * width;
+
+    cout << "Area = " <<area <<endl;
+ 
+
+
+    return 0;
+}
